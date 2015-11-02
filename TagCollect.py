e3e1c806bef3c62933a806ff0fd1fc2eb3fa2a99
tapjdey <tapjdey@users.noreply.github.com>
2015-11-02 19:39:13 +0000

    Oblivion tags collected

--- TagCollect.py
+++ TagCollect.py
@@ -21,7 +21,7 @@ class StopWatch:
         ctime = time.time()+self.mtime
         while(ctime > time.time()):
             pass
-with open('skyrim_mods_all.json','r') as jfile:
+with open('oblivion_mods_all.json','r') as jfile:
     mod_json = json.loads(jfile.read())
     
 
@@ -29,8 +29,8 @@ with open('skyrim_mods_all.json','r') as jfile:
 timer = StopWatch(0.2)
 timer2 = StopWatch(10)
 
-if os.path.isfile('checked.txt'):
-    with open('checked.txt','r') as f:
+if os.path.isfile('checked_o.txt'):
+    with open('checked_o.txt','r') as f:
         checked = f.read().split(',')
 else:
     checked = []
@@ -45,7 +45,7 @@ try:
         
         if str(key) not in checked:
             mod_tag = {}
-            url = 'http://www.nexusmods.com/skyrim/ajax/modtags/?id='+str(key)+'&pUp=1&gid=110'
+            url = 'http://www.nexusmods.com/oblivion/ajax/modtags/?id='+str(key)
             timer.start()
             
             if (k%500) == 0:
@@ -85,21 +85,21 @@ try:
             fstr += '"'+str(key) + '":' + (str(taglist).replace('\'','"')).replace('"s', "'s")+','
 
     fstr = fstr[:-1]+'}'
-    print (checked)
+    
     checked = list(set(checked))
-    print ('l')
+    
     checked_str = ','.join(str (k) for k in checked)
-    with open ('taglist.json','a') as tagfile:
+    with open ('taglist_o.json','a') as tagfile:
         tagfile.write(fstr)
-    with open('checked.txt','w') as f:
+    with open('checked_o.txt','w') as f:
             f.write(checked_str)
         
 except:
     fstr = fstr[:-1]
     checked_str = ','.join(str (k) for k in checked)
-    with open ('taglist.json','a') as tagfile:
+    with open ('taglist_o.json','a') as tagfile:
         tagfile.write(fstr)
-    with open('checked.txt','w') as f:
+    with open('checked_o.txt','w') as f:
         f.write(checked_str)
        
     print ('Connection Error !!!!!!!')
