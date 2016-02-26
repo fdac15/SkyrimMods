e8f55adbe7955b487701c4580d3e4a62da437380
jalomas7 <jmasse10@vols.utk.edu>
2016-02-26 15:48:39 -0500

    semantic and spelling fixes

--- scripts/fishertest/Mod_Has_Tag.py
+++ scripts/fishertest/Mod_Has_Tag.py
@@ -9,7 +9,7 @@ import json, numpy, sys, csv
 
 if(len(sys.argv) < 3):
     print("not enough arguments given")
-    print("usage: python Mod_Has_Tag.py taglist.json taglist_by_mod.json output.csv")
+    print("usage: python Mod_Has_Tag.py taglist.json output.csv")
     exit()
 
 #get tags and mods
@@ -19,23 +19,21 @@ data = {}
 
 with open(sys.argv[1]) as file:
     data = json.load(file)
-    mods = tuple(data.keys())
-    tag_set = set({e for taglist in data.values() for e in taglist})
+
+mods = tuple(data.keys())
+tag_set = set({e for taglist in data.values() for e in taglist})
 
 #generate table
 bools = []
 for mod in mods:
     temp = []
     for tag in tag_set:
-        if tag in data[mod]:
-            temp.append(1)
-        else:
-            temp.append(0)
+        temp.append(1) if tag in data[mod] else temp.append(0)
+
     bools.append(temp)
 
 #write table to file
 writer = csv.DictWriter(open(sys.argv[2],'w'), fieldnames=list(tag_set))
-
 writer.writeheader()
 for row in bools:
     writer.writerow({list(tag_set)[i]: row[i] for i in range(len(row))})
