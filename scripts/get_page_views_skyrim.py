0e72919833ef3d3cea5f5a086ceb0b6f1945f132
jalomas7 <jmasse10@vols.utk.edu>
2016-02-25 23:15:02 -0500

    adding files for fisher testing, and cleaned up directories

--- scripts/get_page_views_skyrim.py
+++
@@ -1,5 +0,0 @@
-import NexusScraper
-
-mods = NexusScraper.json_to_modblock('skyrim_mods_all.json')
-
-NexusScraper.pageviews_to_json(mods, start=0, end=len(mods), name='skyrim_pageviews_all.json', verbose=True)
