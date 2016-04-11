d2189daa131f154d608f2eff38971c8f3e759f03
tapjdey <tapjdey@users.noreply.github.com>
2016-04-11 15:57:23 -0400

    Final Tag Cloud pics updated

---
+++ scripts/word_cloud_table_gen.py
@@ -0,0 +1,19 @@
+'''takes two  command line arguments, the .json filename of a tagl_count file and an output file.
+   creates a string with no. of occurrence as in the count
+'''
+
+import json, sys, re, math
+
+if len(sys.argv) < 3:
+  print('not enough arguments')
+
+with open(sys.argv[1]) as file:
+  data = json.load(file)
+
+outstr = ''
+
+for key in data.keys():
+    outstr += str(key) + ':' + str(data[key]) + '\n'
+
+with open(sys.argv[2], 'w') as file:
+  file.write(outstr)
